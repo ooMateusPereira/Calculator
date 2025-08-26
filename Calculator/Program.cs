05ce831e809dcdf648da1e33013013703e1ad00a
Mateus Pereira da Silva <mateusufc1@live.com>
2025-08-26 15:50:36 -0300

    feat: encapsulation of the sum function

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,11 +6,20 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Soma();
+        }
+        static void Soma()
+        {
             Console.Clear();
             Console.WriteLine("First Value: ");
             float v1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(v1); 
+            Console.WriteLine("Second Value: ");
+            float v2 = float.Parse(Console.ReadLine());
+            float sum = v1 + v2;
+
+
+            Console.WriteLine($"The result of {v1} + {v2} is: {sum}.");
         }
     }
 }
